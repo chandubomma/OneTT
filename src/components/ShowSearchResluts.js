f1972d562287c7d74cb2bfc8e41da47498af2c75
chandubomma <bommachandugoud@gmail.com>
2023-07-28 23:15:16 +0530

    added - sort functionality and language filters.

--- src/components/ShowSearchResluts.js
+++ src/components/ShowSearchResluts.js
@@ -1,23 +1,34 @@
 'use client'
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import MovieCard from "./MovieCard"
 import SortAndFilters from "./SortAndFilters"
 
 const ShowSearchResluts = ({searchResults}) => {
     const [sortBy,setSortBy] = useState("none");
-    const [language,setLanguage] = useState("All");
+    const [language,setLanguage] = useState({All:true});
     const [category,setCategory] = useState("All");
+    const [genresFilter,setGenresFilter] = useState({});
     const [rating,setRating] = useState(0);
     const [runtime,setRuntime] = useState(0);
-
+  var results = [...searchResults];
+  sortByHandler(sortBy,results);
+  results=categoryHandler(category,results);
+  results=languageHandler(language,results);
   return (
     <div className="flex md:flex-row flex-col">
       <div className="">
-        <SortAndFilters/>
+        <SortAndFilters genres={genres}
+          setSortBy={setSortBy}
+          setLanguage={setLanguage}
+          setCategory={setCategory}
+          setGenresFilter={setGenresFilter}
+          language={language}
+          genresFilter={genresFilter}
+        />
       </div>
       <div className="  w-full">
         <div className='grid grid-cols-2 md:grid-cols-4 gap-x-6 md:gap-y-6 gap-y-1 w-fit m-auto pt-5 justify-items-center justify-center'>
-        {searchResults.map(result=>(
+        {results.map(result=>(
             <MovieCard key={result.id} movie={result}/>
         ))}
         </div>
@@ -29,3 +40,139 @@ const ShowSearchResluts = ({searchResults}) => {
 }
 
 export default ShowSearchResluts
+
+function sortByHandler(sortBy,searchResults){
+  switch(sortBy){
+    case 'RD' : {
+        searchResults.sort((m1,m2)=>m2.vote_average-m1.vote_average);
+        break;
+    }
+    case 'RA' : {
+      searchResults.sort((m1,m2)=>m1.vote_average-m2.vote_average);
+      break;
+    }
+    case 'TAZ': {
+      searchResults.sort((m1,m2)=>(m1.title?m1.title:m1.name).localeCompare(m2.title?m2.title:m2.name));
+      break;
+    }
+    case 'TZA': {
+      searchResults.sort((m2,m1)=>(m1.title?m1.title:m1.name).localeCompare(m2.title?m2.title:m2.name));
+      break;
+    }
+    case 'OAF' : {
+      searchResults.sort((m1,m2)=>(m1.release_date?m1.release_date:m1.first_air_date).localeCompare(m2.release_date?m2.release_date:m2.first_air_date));
+      break;
+    }
+    case 'NAF' : {
+      searchResults.sort((m2,m1)=>(m1.release_date?m1.release_date:m1.first_air_date).localeCompare(m2.release_date?m2.release_date:m2.first_air_date));
+      break;
+    }
+  }
+}
+
+function categoryHandler(category,results){
+  switch(category){
+    case 'movies' : {
+      results = results.filter((result)=>result.title!=null);
+      break;
+    }
+    case 'shows' : {
+      results = results.filter((result)=>result.name!=null);
+      break;
+    }
+  }
+  return results
+}
+
+function languageHandler(language,results){
+  if(language.All==true)return results;
+  const newResults=[]
+  Object.keys(language).forEach(lan=>{
+    if(language[lan]==true){
+      results.forEach(result=>{
+        if(result.original_language==lan)newResults.push(result);
+      })
+    }
+  })
+  return newResults;
+}
+
+const genres= [
+  {
+    "id": 28,
+    "name": "Action"
+  },
+  {
+    "id": 12,
+    "name": "Adventure"
+  },
+  {
+    "id": 16,
+    "name": "Animation"
+  },
+  {
+    "id": 35,
+    "name": "Comedy"
+  },
+  {
+    "id": 80,
+    "name": "Crime"
+  },
+  {
+    "id": 99,
+    "name": "Documentary"
+  },
+  {
+    "id": 18,
+    "name": "Drama"
+  },
+  {
+    "id": 10751,
+    "name": "Family"
+  },
+  {
+    "id": 14,
+    "name": "Fantasy"
+  },
+  {
+    "id": 36,
+    "name": "History"
+  },
+  {
+    "id": 27,
+    "name": "Horror"
+  },
+  {
+    "id": 10402,
+    "name": "Music"
+  },
+  {
+    "id": 9648,
+    "name": "Mystery"
+  },
+  {
+    "id": 10749,
+    "name": "Romance"
+  },
+  {
+    "id": 878,
+    "name": "Science Fiction"
+  },
+  {
+    "id": 10770,
+    "name": "TV Movie"
+  },
+  {
+    "id": 53,
+    "name": "Thriller"
+  },
+  {
+    "id": 10752,
+    "name": "War"
+  },
+  {
+    "id": 37,
+    "name": "Western"
+  }
+
+]
--- src/components/SortAndFilters.js
+++ src/components/SortAndFilters.js
@@ -1,18 +1,42 @@
-import React from 'react'
+'use client'
 
-const SortAndFilters = () => {
+const SortAndFilters = ({genres,
+                        setSortBy,
+                        setLanguage,
+                        setCategory,
+                        setRating,
+                        language,
+                        genresFilter,
+                        setGenresFilter
+                        }) => {
+    const handleLanguage = (e)=>{
+        setLanguage({
+            ...language,
+            [e.target.value] : e.target.checked
+        })
+        
+    }
+
+    const handleGenres = ()=>{
+        setGenresFilter({
+            ...genresFilter,
+            [e.val]:!genresFilter[e.val]
+        })
+    }
+   
   return (
+
     <div className='flex md:flex-col md:w-[20rem] w-screen h-fit px-8 pr-10 py-6 pb-16 bg-gradient-to-r from-blue-300 to-gray-300 pt-12
             dark:text-white ml-5 rounded-2xl shadow-lg shadow-gray-400 dark:bg-gradient-to-l dark:from-slate-600 dark:to-blue-900 dark:shadow-black'>
       <div className='w-full font-semibold text-gray-500 text-md'>
-        <select defaultValue="none" className='border-4 border-white focus:outline-none p-2 rounded-lg block w-full'>
+        <select defaultValue="none"  className='border-4 border-white focus:outline-none p-2 rounded-lg block w-full' onClick={(e)=>setSortBy(e.target.value)}>
             <option value="none">--Sort By--</option>
             <option value="RD">Rating Descending</option>
             <option value="RA">Rating Ascending</option>
             <option value="NAF">New Arrivals First</option>
             <option value="OAF">Old Arrivals First</option>
             <option value="TAZ">Title (A-Z)</option>
-            <option value="TZA">Title (z-A)</option>
+            <option value="TZA">Title (Z-A)</option>
         </select>
       </div>
       <div className='md:mt-5'>
@@ -21,43 +45,58 @@ const SortAndFilters = () => {
             <h3 className='md:text-md font-semibold text-gray-600'>Language</h3>
            <div className='grid grid-cols-2 gap-y-2 pl-2 text-md font-medium text-gray-500 p-2 py-4'>
                 <div className=''>
-                    <input type='checkbox' id='All' value="All" className='w-4 h-4 mr-2' checked />
-                    <label forHtml="All">All</label>
+                    <input type='checkbox' id='All' value="All" className='w-4 h-4 mr-2' defaultChecked onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="All">All</label>
                 </div>
                 <div>
-                    <input type='checkbox' id='en' value="en" className='w-4 h-4 mr-2'/>
-                    <label forHtml="en">English</label>
+                    <input type='checkbox' id='en' value="en" className='w-4 h-4 mr-2' onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="en">English</label>
                 </div>
                 <div>
-                    <input type='checkbox' id='hi' value="hi" className='w-4 h-4 mr-2'/>
-                    <label forHtml="hi">Hindi</label>
+                    <input type='checkbox' id='hi' value="hi" className='w-4 h-4 mr-2' onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="hi">Hindi</label>
                 </div>
                 <div>
-                    <input type='checkbox' id='kn' value="kn" className='w-4 h-4 mr-2'/>
-                    <label forHtml="kn">Kannada</label>
+                    <input type='checkbox' id='kn' value="kn" className='w-4 h-4 mr-2' onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="kn">Kannada</label>
                 </div>
                 <div>
-                    <input type='checkbox' id='ml' value="ml" className='w-4 h-4 mr-2'/>
-                    <label forHtml="ml">Malyalam</label>
+                    <input type='checkbox' id='ml' value="ml" className='w-4 h-4 mr-2' onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="ml">Malyalam</label>
                 </div>
                 <div>
-                    <input type='checkbox' id='ta' value="ta" className='w-4 h-4 mr-2'/>
-                    <label forHtml="ta">Tamil</label>
+                    <input type='checkbox' id='ta' value="ta" className='w-4 h-4 mr-2' onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="ta">Tamil</label>
                 </div>
                 <div>
-                    <input type='checkbox' id='te' value="te" className='w-4 h-4 mr-2'/>
-                    <label forHtml="te">Telugu</label>
+                    <input type='checkbox' id='te' value="te" className='w-4 h-4 mr-2' onChange={(e)=>handleLanguage(e)}/>
+                    <label forhtml="te">Telugu</label>
                 </div>
            </div>
         </div>
         <div className='w-full font-semibold text-gray-500 text-md md:mt-3'>
             <h1 className='text-gray-600 pl-2 font-semibold mb-2'>Category</h1>
-            <select defaultValue='All' className='border-4 border-white focus:outline-none p-2 rounded-lg block w-full'>
-                <option value='All'>All</option>
+            <select defaultValue="All" className='border-4 border-white focus:outline-none p-2 rounded-lg block w-full' onClick={(e)=>setCategory(e.target.value)}>
+                <option value='All' selected>All</option>
                 <option value='movies'>Movies</option>
                 <option value='shows'>TV Shows</option>
             </select>
         </div>
+
+        {
+            genres?
+            <div className='w-full mt-5 pl-2 text-gray-600 font-semibold text-md'>
+                <h3 className=''>Genres</h3>
+                <div className='flex flex-wrap w-full mt-3'>
+                    {
+                        genres.map(genre=>(
+                            <button key={genre.id} value={genre.name} onClick={(e)=>handleGenres(e)} className='border-2 p-2 px-4 text-gray-500 bg-none rounded-2xl mx-2 my-1 border-gray-400'>{genre.name}</button>
+                        ))
+                    }
+                </div>
+            </div>:''
+        }
+
         <div className='w-full font-semibold text-gray-600 text-md md:mt-5'>
             <h3 className='pl-2'>Rating</h3>
             <input type='range' min={0} max={100} className='w-full' />
